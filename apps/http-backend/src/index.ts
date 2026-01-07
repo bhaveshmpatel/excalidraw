a99342f4b98f9a1df78eef7688dcf72d9b2e7030
Bhavesh Patel <bhaveshmpatel2005@gmail.com>
2026-01-07 16:57:13 +0530

    added signup endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,17 +3,32 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { CreateUserSchema, SiginSchema, CreateRoomSchema } from "@repo/common/types";
 import { middleware } from "./middleware";
+import { prismaClient } from "@repo/db/client";
 
 const app = express();
 app.use(express.json());
 
-app.post("/signup", (req, res) => {
-  const data = CreateUserSchema.safeParse(req.body);
-  if (!data.success) {
+app.post("/signup", async (req, res) => {
+  const parsedData = CreateUserSchema.safeParse(req.body);
+  if (!parsedData.success) {
     res.status(411).json({
       message: "Incorrect Inputs",
     });
     return;
+  }
+
+  try {
+    await prismaClient.user.create({
+      data: {
+        email: parsedData.data?.username,
+        password: parsedData.data.password,
+        name: parsedData.data.name,
+      },
+    });
+  } catch (error) {
+    res.status(411).json({
+      message: "User already exist with this username"
+    })
   }
 });
 
@@ -31,9 +46,8 @@ app.post("/signin", (req, res) => {
   const token = jwt.sign(username, JWT_SECRET);
 
   res.json({
-    token
-  })
-
+    token,
+  });
 });
 
 app.post("/room", (req, res) => {
---
+++ packages/db/prisma/migrations/20260107105745_made_photo_optional/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "photos" DROP NOT NULL;
